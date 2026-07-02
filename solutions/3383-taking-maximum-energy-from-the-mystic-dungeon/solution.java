bfc67e7ce646a05b87038377363710e3ae6567c0
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2026-07-02 07:02:41 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 128.4 MB (81.40%)

--- solutions/3383-taking-maximum-energy-from-the-mystic-dungeon/solution.java
+++ solutions/3383-taking-maximum-energy-from-the-mystic-dungeon/solution.java
@@ -1,34 +1,13 @@
-public class Solution 
-{
-    public int maximumEnergy(int[] energy, int k) 
-    {
-        int n = energy.length;
-        int[] maxSum = new int[k];
-        int[] currentSum = new int[k];
-        
-        for (int i = 0; i < k; i++) 
-        {
-            maxSum[i] = Integer.MIN_VALUE;
-        }
-        
-        for (int i = n - 1; i >= 0; i--) 
-        {
-            int r = i % k;
-            currentSum[r] += energy[i];
-            if (currentSum[r] > maxSum[r]) 
-            {
-                maxSum[r] = currentSum[r];
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int maxEnergy = Integer.MIN_VALUE;
+        for(int i=energy.length-k;i<energy.length;i++){
+            int sum = 0;
+            for(int j=i;j>=0;j-=k){
+                sum += energy[j];
+                maxEnergy = Math.max(sum, maxEnergy);
             }
         }
-        int result = maxSum[0];
-        for (int i = 1; i < k; i++) 
-        {
-            if (maxSum[i] > result) 
-            {
-                result = maxSum[i];
-            }
-        }
-        
-        return result;
+        return maxEnergy;
     }
 }
