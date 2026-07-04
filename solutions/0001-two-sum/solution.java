6ee8ac77fbb46021be40b782d082574ad29d2d6a
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2026-07-04 15:18:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.2 MB (23.42%)

--- solutions/0001-two-sum/solution.java
+++ solutions/0001-two-sum/solution.java
@@ -2,19 +2,18 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target) 
     {
-        int[] arr = new int[2];
-        for(int i =0; i<nums.length; i++)
+        for(int i =1; i<nums.length; i++)
         {
-            for(int j = i+1; j<nums.length; j++)
+            for(int j = i; j<nums.length; j++)
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    arr[0] = i;
-                    arr[1] = j;
-                    break;
-                }
+                if(nums[j-i] + nums[j] == target) return new int[]{j-i,j};
+                // {
+                //     arr[0] = i;
+                //     arr[1] = j;
+                //     break;
+                // }
             }
         }
-        return arr;
+        return new int[]{};
     }
 }
