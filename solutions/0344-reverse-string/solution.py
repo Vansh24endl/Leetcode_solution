9f250f4b2318c38604ea51268346fa0a2b3bd48b
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2026-07-09 05:52:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.7 MB (18.97%)

--- solutions/0344-reverse-string/solution.py
+++ solutions/0344-reverse-string/solution.py
@@ -3,4 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        # a = 0
+        # b = len(s) -1
+
+        # while a < b:
+        #     s[a],s[b] = s[b],s[a]
+        #     a += 1
+        #     b -= 1
+
         s.reverse()
